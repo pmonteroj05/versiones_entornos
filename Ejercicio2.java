cadfce6a79461fab3d359d327abae095564cc619
Paula Montero Jiménez <pmonteroj05@iesalbarregas.es>
2026-02-05 22:30:46 +0100

    Ejercicio 2 Versión 1

---
+++ Ejercicio2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    static Scanner numeros = new Scanner(System.in);
+    public static void main(String[] args){
+        int edades[] = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Introduce una edad: ");
+            edades[i] = numeros.nextInt();
+        }
+
+        System.out.print("Edades introducidas: ");
+        for (int i = 0; i < 5; i++)
+            System.out.print(edades[i] + " ");
+    }
+}
