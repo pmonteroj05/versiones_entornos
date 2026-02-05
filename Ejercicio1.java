e29e0879b53d5c8374f786eb82b16120310257b3
Paula Montero Jiménez <pmonteroj05@iesalbarregas.es>
2026-02-05 22:12:00 +0100

    Ejercicio 1 Versión 2

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -5,16 +5,21 @@ public class Ejercicio1 {
     static Scanner numeros = new Scanner(System.in);
 
     public static void main(String[] args){
-
         int n [] = new int [5];
+        int suma = 0;
 
         for (int i = 0; i < 5; i++) {
             System.out.print("Introduce un número: ");
             n[i] = numeros.nextInt();
+            suma += n[i];
         }
+
+        double media = (double) suma / 5;
 
         System.out.print("Números introducidos: ");
         for (int i = 0; i < 5; i++)
             System.out.print(n[i] + " ");
+
+        System.out.printf("Suma: %d  |  Media: %f", suma, media);
     }
 }
